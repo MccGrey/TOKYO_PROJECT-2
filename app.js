1b86ad305b494cb6544dfbb6587ae44dadb0fe00
MccGrey <chuxzzie01@yahoo.com>
2022-09-05 15:25:57 +0100

    NAVBAR, MCCGREY SECTION, ABOUT PAGE 99% DONE

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+//=============RESPONSIVE NAVBAR
 
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+//function for open nav bar
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+//function for close nav bar
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+//=======FLIP EFFECT
+let btns = document.querySelectorAll(".flip");
+let btn2 = document.querySelectorAll(".flp");
+
+btns.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const cc = event.target.parentElement.parentElement;
+    cc.classList.toggle("flipped");
+  });
+});
+
+btn2.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const cc = event.target.parentElement.parentElement;
+    cc.classList.toggle("flipped");
+  });
+});
