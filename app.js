8523373771db75350f3ce6cb047c96167e738c2b
MccGrey <chuxzzie01@yahoo.com>
2022-09-07 15:05:45 +0100

    project tokyo complete

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
+//==========================================RESPONSIVE NAVBAR=====================================================
 
-        $(function() {
-            $(".toggle").on("click", function(){
-                if($(".item").hasClass("active")){
-                    $(".item").removeClass("active");
-                }
-                else{
-                    $(".item").addClass("active");
-                }
-            })
-        });
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+//=====================================FUNCTION FOR OPEN NAVBAR=================================================
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+//========================================FUNCTION FOR CLOSE NAVBAR=================================================
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+//================================FLIP EFFECT FOR  DIRECTOR SECTION========================================
+let btns = document.querySelectorAll(".flip");
+let btn2 = document.querySelectorAll(".flp");
+
+btns.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const cc = event.target.parentElement.parentElement;
+    cc.classList.toggle("flipped");
+  });
+});
+
+btn2.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const cc = event.target.parentElement.parentElement;
+    cc.classList.toggle("flipped");
+  });
+});
